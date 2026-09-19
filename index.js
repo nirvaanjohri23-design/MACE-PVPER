903293f6f2ecc603fd3c91b1aae7c01f81c3320e
nirvaanjohri23-design <nirvaanjohri23@gmail.com>
2026-09-19 16:34:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1224,7 +1224,7 @@ function createBot() {
     });
 
     bot.loadPlugin(pathfinder);
-bot.on('spawn', () => { bot.equip('shield', 'off-hand'); setTimeout(() => { bot.setControlState('sneak', true); bot.activateItem(); }, 1000); });
+bot.once('spawn', () => { bot.equip('shield', 'off-hand'); setInterval(() => { const player = bot.nearestEntity(e => e.type === 'player'); if (player) bot.lookAt(player.position.offset(0, player.position.y - bot.entity.position.y + 1.62, 0)); }, 1000); });
     // FIX: connection timeout - end the old bot before reconnecting to avoid ghost bots
     clearBotTimeouts();
     connectionTimeoutId = setTimeout(() => {
